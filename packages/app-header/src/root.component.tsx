5612b38b333099d2dc453ac70596d6966c28e450
cybermaique <maiqued.18@gmail.com>
2022-12-01 14:54:05 -0300

    feat(header): logo, icon and header finish

--- packages/app-header/src/root.component.tsx
+++ packages/app-header/src/root.component.tsx
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { Navigate } from "@shoppe/util-ui";
+import { Header } from "@shoppe/util-ui";
 
 // @ts-ignore
 import { useStore } from "@shoppe/util-state";
@@ -7,14 +7,5 @@ import { useStore } from "@shoppe/util-state";
 export default function Root(props) {
   const store = useStore();
 
-  return (
-    <>
-      <section>HEADER</section>
-      <h3>Counter ::: {store.counter}</h3>
-      <Navigate path="/account">Account</Navigate>
-      <Navigate path="/cart">Cart</Navigate>
-      <Navigate path="/product">Product</Navigate>
-      <Navigate path="/shop">Shop</Navigate>
-    </>
-  );
+  return <Header />;
 }
